2cc65f60908695f3b755b25b673d14b2ce8c37d5
Ayush Sehrawat <itsayushsehrawat@gmail.com>
2023-06-23 20:58:52 +0530

    feat: seo changes

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -47,6 +47,26 @@
 	});
 </script>
 
+<svelte:head>
+	<title>GoalBash</title>
+	<meta name="title" content="GoalBash" />
+	<meta name="description" content="GoalBash is a goal tracking app that helps you achieve your goals by breaking them down into small actionable steps." />
+
+	<!-- Open Graph / Facebook -->
+	<meta property="og:type" content="website" />
+	<meta property="og:url" content="https://goalbash.neobrains.dev" />
+	<meta property="og:title" content="GoalBash" />
+	<meta property="og:description" content="GoalBash is a goal tracking app that helps you achieve your goals by breaking them down into small actionable steps." />
+	<meta property="og:image" content="https://metatags.io/images/meta-tags.png" />
+
+	<!-- Twitter -->
+	<meta property="twitter:card" content="summary_large_image" />
+	<meta property="twitter:url" content="https://goalbash.neobrains.dev" />
+	<meta property="twitter:title" content="GoalBash" />
+	<meta property="twitter:description" content="GoalBash is a goal tracking app that helps you achieve your goals by breaking them down into small actionable steps." />
+	<meta property="twitter:image" content="https://goalbash.neobrains.dev/goalbash.png" />
+</svelte:head>
+
 <Hero />
 <About />
 <Features />
